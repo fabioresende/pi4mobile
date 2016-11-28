93ccf1efbe229c6c978174faf70078b34ed3262b
angoti <angoti@gmail.com>
2016-11-28 19:21:08 -0200

    Navigation Drawer com opções de LongClick para excluir ou alterar itens

--- app/src/main/java/professorangoti/com/artistas/activities/ListaTodos2.java
+++ app/src/main/java/professorangoti/com/artistas/activities/ListaTodos2.java
@@ -2,8 +2,14 @@ package professorangoti.com.artistas.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -23,12 +29,44 @@ import retrofit2.Response;
 public class ListaTodos2 extends AppCompatActivity {
 
     Context contexto = this;
+    ListView listView;
+    List<Artista> dados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listatodos);
+        listView = (ListView) findViewById(R.id.minha_lista);
         consultaServidor();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        menu.setHeaderTitle("Opções");
+        menu.add(0, dados.get(info.position).getCodArtista(), dados.get(info.position).getCodArtista(), "Excluir");
+        menu.add(0, dados.get(info.position).getCodArtista(), dados.get(info.position).getCodArtista(), "Alterar");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getTitle() == "Excluir") {
+            function1(item.getItemId());
+        } else if (item.getTitle() == "Alterar") {
+            function2(item.getItemId());
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public void function1(int id) {
+        Toast.makeText(this, "Chamar Activity para excluir item " + id, Toast.LENGTH_LONG).show();
+    }
+
+    public void function2(int id) {
+        Toast.makeText(this, "Chamar Activity para alterar item " + id, Toast.LENGTH_LONG).show();
     }
 
     public void consultaServidor() {
@@ -40,12 +78,13 @@ public class ListaTodos2 extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<Artista>> chamada, Response<List<Artista>> resposta) {
                 if (resposta.isSuccessful()) {
-                    List<Artista> dados = resposta.body();
+                    dados = resposta.body();
                     //verifica aqui se o corpo da resposta não é nulo
                     if (dados != null) {
-                        ListView listView = (ListView) findViewById(R.id.minha_lista);
+
                         final ListaTodosAdaptador adaptador = new ListaTodosAdaptador(contexto, dados);
                         listView.setAdapter(adaptador);
+                        registerForContextMenu(listView);
                     } else {
                         Toast.makeText(getApplicationContext(), "Resposta nula do servidor", Toast.LENGTH_SHORT).show();
                     }
